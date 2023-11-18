84ca27b04230a362622d4dde55b648c41c43c486
Zain Ashraf <zainashraf2001@gmail.com>
2023-11-18 14:20:15 -0800

    begin app, create 3 buttons and connect UI elements to code

---
+++ Inviter-App-xCode/Inviter-App-xCode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Inviter-App-xCode
+//
+//  Created by Enduser on 11/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func messageTextField(_ sender: UITextField) {
+    }
+    
+    @IBAction func phoneNumberTextField(_ sender: UITextField) {
+    }
+    
+    @IBAction func sendButton(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ Inviter-App-xCode/Inviter-App-xCodeUITests/Inviter_App_xCodeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Inviter_App_xCodeUITestsLaunchTests.swift
+//  Inviter-App-xCodeUITests
+//
+//  Created by Enduser on 11/17/23.
+//
+
+import XCTest
+
+final class Inviter_App_xCodeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
