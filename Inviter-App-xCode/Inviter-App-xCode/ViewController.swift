b42626be30c22d78d37749c8050bbd1643c2f2db
Zain Ashraf <zainashraf2001@gmail.com>
2023-11-18 16:31:00 -0800

    assign delegate for MFMessageComposeViewController in ViewController

--- Inviter-App-xCode/Inviter-App-xCode/ViewController.swift
+++ Inviter-App-xCode/Inviter-App-xCode/ViewController.swift
@@ -6,11 +6,26 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,MFMessageComposeViewControllerDelegate {
     
-
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        controller.dismiss(animated: true) {
+                    switch result {
+                    case .sent:
+                        print("Message sent successfully!")
+                    case .cancelled:
+                        print("Message sending cancelled.")
+                    case .failed:
+                        print("Message sending failed.")
+                    @unknown default:
+                        fatalError("New case added in MessageComposeResult.")
+                    }
+                }
+            }
+    
  
     @IBOutlet weak var messageTextField: UITextField!
     
@@ -18,12 +33,27 @@ class ViewController: UIViewController {
     
     @IBAction func sendButton(_ sender: UIButton) {
         guard let message = messageTextField.text, !message.isEmpty, let numbers = phoneNumberTextField.text?.components(separatedBy: ",").map({ $0.trimmingCharacters(in: .whitespacesAndNewlines) }),
-             // everything above this retrieves the message and phone numbers
-                !numbers.isEmpty else {
-                    print("message or phone number is empty.")
-                    return
+              // everything above this retrieves the message and phone numbers
+              !numbers.isEmpty else {
+            print("message or phone number is empty.")
+            return
         } // this else statements handles a case where the number is empty
+        
+        for number in numbers {
+            //            sendSMS(message: message, recipiennt: number)
+        }
     }
+        
+        func sendSMS(message: String, recipient: String) {
+            if MFMessageComposeViewController.canSendText() {
+                let messageController = MFMessageComposeViewController()
+                messageController.body = message
+                messageController.recipients = [recipient]
+                messageController.messageComposeDelegate = self
+                
+                present(messageController, animated: true)
+            }
+        }
     
     override func viewDidLoad() {
         super.viewDidLoad()
