44d312f3409e6292b04a52118e6e2dcda22d7226
Zain Ashraf <zainashraf2001@gmail.com>
2023-11-26 20:55:39 -0800

    8 second delay instead of 4

--- Inviter-App-xCode/Inviter-App-xCode/ViewController.swift
+++ Inviter-App-xCode/Inviter-App-xCode/ViewController.swift
@@ -69,7 +69,7 @@ class ViewController: UIViewController,MFMessageComposeViewControllerDelegate, U
         sendSMS(message: message, recipient: recipient)
         
         // adds delay between each message being sent
-        DispatchQueue.main.asyncAfter(deadline: .now() + 4.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 8.0) {
             self.sendNextMessage(index + 1, numbers: numbers, message: message)
         }
         
